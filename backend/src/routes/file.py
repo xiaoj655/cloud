ea99afb5ea5585d36041402f4705c99e88da1735
jkl <xiaoj655@gmail.com>
2024-09-06 20:05:28 +0800

    feat  图片服务端压缩

--- backend/src/routes/file.py
+++ backend/src/routes/file.py
@@ -9,6 +9,8 @@ import shutil
 from fastapi.responses import FileResponse
 from src import models
 import hashlib
+import io
+from PIL import Image
 
 router = APIRouter()
 data_dir = os.path.join(os.getcwd(), 'data')
@@ -23,6 +25,11 @@ async def get(id: UUID = Path(...)):
     else:
         return Response(status_code=404)
 
+
+def save_bytesio_to_file(bytesio_obj: io.BytesIO, output_path: str):
+    with open(output_path, 'wb') as f:
+        f.write(bytesio_obj.getvalue())
+
 @router.post('/file')
 async def post(token: Annotated[str, Depends(auth.oauth2_scheme)], files: list[UploadFile]):
     user = auth.get_identity(token)
@@ -35,10 +42,18 @@ async def post(token: Annotated[str, Depends(auth.oauth2_scheme)], files: list[U
             file_md5.update(chunk)
         file_md5 = file_md5.hexdigest()
         await file.seek(0)
+        file_content = await file.read()
+        file.seek(0)
+        img = Image.open(io.BytesIO(file_content))
+        img_byte_arr = io.BytesIO()
+        img.save(img_byte_arr, format='webp', quality=80)
+        img_byte_arr.seek(0)
+
         file_id = str(uuid4())[:8]
         file_path = os.path.join(data_dir, user_id, file_id + '.' + file.filename.split('.')[-1])
-        with open(file_path, 'wb') as buffer:
-            shutil.copyfileobj(file.file, buffer)
+        save_bytesio_to_file(img_byte_arr, file_path)
+        # with open(file_path, 'wb') as buffer:
+        #     shutil.copyfileobj(file.file, buffer)
         # file_item = File(file_type=file.filename.split('.')[-1], file_name=file.filename, publisher_id=user_id)
         _id = uuid4()
         item = {
